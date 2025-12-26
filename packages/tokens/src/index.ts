d67457ae5bbdb3d8ec1a9b1bc1d00ece8e50d169
Prathamesh Dharmadhikari <prathameshdharmadhikari3@gmail.com>
2025-12-26 23:14:18 +0530

    setup typescript monorepo packages

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    white : "#ffffff",
+    black : "#000000",
+    primary:"#4f46e5"
+
+}
+
+
+export const spacing = {
+    xs : "4px",
+    sm : "8px",
+    md : "16px",
+    lg : "24px"
+}
